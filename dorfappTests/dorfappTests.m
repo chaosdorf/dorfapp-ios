2924d314c50072b7a277e5f57ac09e7fb9f42948
r4mp <r4mp@chaosdorf.de>
2013-06-07 22:15:23 +0200

    Inital commit

---
+++ dorfappTests/dorfappTests.m
@@ -0,0 +1,44 @@
+//
+//  dorfappTests.m
+//  dorfappTests
+//
+//  Copyright (C) 2013  Gerrit Giehl <r4mp@chaosdorf.de>
+//
+//  This program is free software: you can redistribute it and/or modify it under
+//  the terms of the GNU General Public License as published by the Free Software
+//  Foundation, either version 3 of the License, or (at your option) any later
+//  version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+//  details.
+//
+//  You should have received a copy of the GNU General Public License along with
+//  this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "dorfappTests.h"
+
+@implementation dorfappTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in dorfappTests");
+}
+
+@end
